959039639196d9dc9bcb4ebdfe72d789932f4472
Vincent2366 <2001105940@student.buksu.edu.ph>
2024-11-12 06:35:48 +0800

    Added front end for fetching all Users

--- client/src/components/dashboard/admin/AdminRoutes.js
+++ client/src/components/dashboard/admin/AdminRoutes.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 // import AdminLayout from './AdminLayout';
 import ManageOrganizations from './ManageOrganizations'; // Ensure this import is correct
+import ManageUsers from './ManageUsers';
 
 const AdminRoutes = () => {
   return (
     //<AdminLayout>
       <Routes>
         <Route path="manage-organizations" element={<ManageOrganizations />} />
-        {/* Add other admin routes here as needed */}
+        <Route path="manage-users" element={<ManageUsers/>}/> 
       </Routes>
-    //</AdminLayout>
+    
   );
 };
 
